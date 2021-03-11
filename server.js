5903909f4eddbc48dca4acd2b39ea7a42fa492cf
Gabriel2644 <74109971+Gabriel2644@users.noreply.github.com>
2021-03-11 13:44:13 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import cards from "./dbCards.js";
+import Cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://admin:37635997@cluster0.nutk3.mongodb.net/tinderdb?retryWrites=true&w=majority";
+
+app.use(express.json());
+app.use(Cors());
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+app.get("/tinder/card", (req, res) => {
+  cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
